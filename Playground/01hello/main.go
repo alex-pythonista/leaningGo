026a848dbbcac29bf5ee0d472587de786090bba8
alex-pythonista <indrajit.alex@gmail.com>
2022-09-10 01:39:39 +0600

    todays learning

---
+++ Playground/01hello/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "AlexPy"
+	fmt.Println(username)
+	fmt.Printf("username is of type: %T \n", username)
+
+	var isActive string
+	fmt.Println(isActive)
+	fmt.Printf("username is of type: %T \n", isActive)
+}
